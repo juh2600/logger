285ee81b543681e2e8512598344656a1b143ca0e
Joe Reed <josephreed2600@gmail.com>
2020-05-06 06:49:21 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Usage:
+ * var logger = require('logger').get('context_name')
+ * logger.log('stuff') // yields a message prefixed with '[context_name]'
+ */
+exports.get = function(role, options = {}) {
+	var defaults = {path: './logs/', level: 'debug'};
+	//for(i in defaults) if(!o.hasOwnProperty(i)) o[i] = defaults[i];
+	o = Object.assign(defaults, options);
+	var winston = require('winston');
+	var log_format = winston.format.printf(
+		info => {info.message = '[' + role + '] ' + info.message; return info;}
+	);
+	var logger = winston.createLogger({
+		level: o.level,
+		format: winston.format.combine(
+			log_format,
+			winston.format.align()
+			),
+		transports: []
+	});
+	logger.add(new winston.transports.File({
+		filename: o.path + role + '-error.log',
+		level: 'error',
+		format: winston.format.simple()
+	}));
+	logger.add(new winston.transports.File({
+		filename: o.path + role + '-combined.log',
+		format: winston.format.simple()
+	}));
+	if(process.env.NODE_ENV !== 'production') {
+		logger.add(new winston.transports.Console({
+			format: winston.format.simple()
+		}));
+	}
+	return logger;
+};
